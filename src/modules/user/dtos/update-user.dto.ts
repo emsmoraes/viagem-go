a3e0fa71aa379e568cabf78af25c30adae19e5c3
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-02-09 03:18:49 -0300

    feat: add password confirmation field in user creation dto

--- src/modules/user/dtos/update-user.dto.ts
+++ src/modules/user/dtos/update-user.dto.ts
@@ -1,5 +1,6 @@
 import { IsString, IsEmail, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Match } from '../validators/match.decorator';
 
 export class UpdateUserDto {
   @ApiProperty({ example: 'John Doe' })
@@ -10,4 +11,9 @@ export class UpdateUserDto {
   @IsString()
   @Length(6, 50)
   password: string;
+
+  @ApiProperty({ example: 'securePassword123' })
+  @IsString()
+  @Match('password', { message: 'Password confirmation does not match' })
+  passwordConfirm: string;
 }
