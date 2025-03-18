509b6d39c461b383e7fec6b9a72fbb18cacd7f73
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-03-18 16:41:02 -0300

    feat: add task to update expired subscriptions in Stripe module

--- src/integrations/stripe/stripe.module.ts
+++ src/integrations/stripe/stripe.module.ts
@@ -3,9 +3,10 @@ import { StripeService } from './stripe.service';
 import { StripeController } from './stripe.controller';
 import { EnvService } from 'src/modules/env/env.service';
 import { PrismaService } from 'src/shared/database/prisma/prisma.service';
+import { ExpireSubscriptionsJob } from './jobs/update-expired-trials.job';
 
 @Module({
-  providers: [StripeService, EnvService, PrismaService],
+  providers: [StripeService, EnvService, PrismaService, ExpireSubscriptionsJob],
   controllers: [StripeController],
 })
 export class StripeModule {}
